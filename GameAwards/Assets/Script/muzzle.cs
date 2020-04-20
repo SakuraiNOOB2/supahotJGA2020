830f9a31ee081ebd41fceb487d60ba6d124e3ebc
Doge-Coin-Tsukae <63012757+Doge-Coin-Tsukae@users.noreply.github.com>
2020-04-20 14:22:50 +0900

    結合した
    プロジェクトがバラバラだったので結合した。足りないところあるかも

---
+++ GameAwards/Assets/Script/muzzle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class muzzle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform MyTransform = this.transform;
+
+        Vector3 pos = MyTransform.position;
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            pos.x += 0.05f;
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            pos.x -= 0.05f;
+        }
+
+        if (Input.GetKey(KeyCode.U))
+        {
+            pos.y += 0.05f;
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            pos.y -= 0.05f;
+        }
+
+        MyTransform.position = pos;
+        pos.x = 0;
+    }
+}
