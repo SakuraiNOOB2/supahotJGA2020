6dc312d80e18303d4d4f5abfd191caffbcfe6d45
Doge-Coin-Tsukae <63012757+Doge-Coin-Tsukae@users.noreply.github.com>
2020-05-15 18:13:54 +0900

    ステージ選択機能実装
    ブロックを押すと設定したステージに応じたステージを呼び出すことが出来ます。
    ブロックの名前をステージ名にすることでステージ選択できます

---
+++ GameAwards/Assets/Script/Stage_Select.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage_Select : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                SceneManager.LoadScene(hit.collider.gameObject.name);
+            }
+        }
+    }
+}
