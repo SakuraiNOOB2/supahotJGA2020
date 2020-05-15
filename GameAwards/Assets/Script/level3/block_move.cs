0848194bf6801e935c71f71c3f64a77cfe67b1f1
LIUC342 <57204658+LIUC342@users.noreply.github.com>
2020-05-15 16:08:19 +0900

    ステージ3修正
    動く床を修正した

--- GameAwards/Assets/Script/level3/block_move.cs
+++ GameAwards/Assets/Script/level3/block_move.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class block_move : MonoBehaviour
 {
     int dirx,diry;
+    bool movedown;
     // Start is called before the first frame update
     void Start()
     {
         dirx = 1;
         diry = 1;
+        movedown = false;
     }
 
     // Update is called once per frame
@@ -23,12 +25,33 @@ public class block_move : MonoBehaviour
         if (now.x > 15)
             dirx = -1;
 
-        if (now.y > 20)
+        if (now.y > 0 && movedown)
             diry = -1;
-        if (now.y < 0)
+        else if (now.y < 10 && !movedown)
             diry = 1;
+        else
+            diry = 0;
 
-        now += new Vector3(0.03f * dirx, 0.03f*4/7*diry, 0.0f);
-        rb.position = now;
+        //now += new Vector3(0.03f * dirx, 0.03f*4/7*diry, 0.0f);
+        //rb.position = now;
+
+        transform.Translate(0.03f * dirx, 0.03f  * diry, 0.0f);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "concrete")
+        {
+            movedown = true;
+            Debug.Log("コンクリートが乗った");
+        }
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "concrete")
+        {
+            movedown = false;
+            Debug.Log("コンクリートが離れた");
+        }
     }
 }
