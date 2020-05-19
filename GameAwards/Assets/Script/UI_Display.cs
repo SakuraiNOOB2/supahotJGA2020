f9c0674ebe1add76253ccd002bc408e09b7cb2fe
Hohman-Y <56616412+Hohman-Y@users.noreply.github.com>
2020-05-19 10:02:57 +0900

    UIに配置した立体を回すだけ

---
+++ GameAwards/Assets/Script/UI_Display.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Display : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ワールドのy軸に沿って1秒間に90度回転
+        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime, Space.World);
+    }
+}
