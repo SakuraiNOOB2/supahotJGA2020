8538f1367447cb0f71fe62dd6d3203387d29a0b3
Doge-Coin-Tsukae <63012757+Doge-Coin-Tsukae@users.noreply.github.com>
2020-05-19 13:03:40 +0900

    カーソルっぽいもの
    コントローラーで動かすカーソル
    (ただしボタンを押してもステージ遷移しない)

---
+++ GameAwards/Assets/Script/cursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dpv = Input.GetAxis("D_Pad_V");
+        float dph = Input.GetAxis("D_Pad_H");
+        Vector3 Pos = transform.position;
+
+        Pos.x = Pos.x + dph / 100;
+        Pos.y = Pos.y + dpv / 100;
+
+        transform.position = Pos;
+    }
+}
