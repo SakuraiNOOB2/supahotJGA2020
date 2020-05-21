e86771354dd6031eabda111792aac75e640791d5
Doge-Coin-Tsukae <63012757+Doge-Coin-Tsukae@users.noreply.github.com>
2020-05-21 14:28:35 +0900

    ステージ選択画面

---
+++ GameAwards/Assets/Script/Stage_Select_Block.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage_Select_Block : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Rot;
+        Rot = this.gameObject.transform.localEulerAngles;
+        Rot.y += 0.1f;
+        this.gameObject.transform.localEulerAngles = Rot;
+    }
+}
