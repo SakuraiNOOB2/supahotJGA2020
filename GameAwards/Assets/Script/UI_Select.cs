bfa48e3f590cf10c3d4ba68afb941b032502d3d1
Hohman-Y <56616412+Hohman-Y@users.noreply.github.com>
2020-05-22 17:08:42 +0900

    UIのブロック表示制御
    現状Cキーで起動

---
+++ GameAwards/Assets/Script/UI_Select.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Select : MonoBehaviour
+{
+    public GameObject UI_Concrete;
+    public GameObject UI_Concrete2;
+
+
+    //生成用フラグ
+    private int flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flag = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(flag)
+        {
+            case 0:
+
+                UI_Concrete.SetActive(true);
+                UI_Concrete2.SetActive(false);
+
+                if(Input.GetKeyDown(KeyCode.C))
+                {
+                    flag = 1;
+                    Debug.Log(flag);
+                }
+
+                break;
+
+            case 1:
+
+                UI_Concrete.SetActive(false);
+                UI_Concrete2.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.C))
+                {
+                    flag = 0;
+                    Debug.Log(flag);
+                }
+
+                break;
+
+            default:
+                Debug.Log("飛ばない");
+
+
+                break;
+
+        }
+        
+    }
+}
