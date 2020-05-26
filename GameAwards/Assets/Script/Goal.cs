6ddc04f139d57f54ae43fdfdc8526e3531e5d232
Doge-Coin-Tsukae <63012757+Doge-Coin-Tsukae@users.noreply.github.com>
2020-05-26 10:54:36 +0900

    戻し
    ゴールスクリプトが消失したので再追加

--- GameAwards/Assets/Script/Goal.cs
+++ GameAwards/Assets/Script/Goal.cs
@@ -2,25 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
+    private bool Clear;
+    private int time;
+
+    [SerializeField]
+    private Text UnkoText; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Clear = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Clear==true)
+        {
+            float alfa = 1.0f;
+            float red = UnkoText.gameObject.GetComponent<Text>().color.r;
+            float green = UnkoText.gameObject.GetComponent<Text>().color.g;
+            float blue = UnkoText.gameObject.GetComponent<Text>().color.b;
+
+            UnkoText.gameObject.GetComponent<Text>().color = new Color(red, green, blue, alfa);
+
+            time++;
+        }
+
+        if(time >=300)
+        {
+            SceneManager.LoadScene("StageSelect");
+        }
     }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "ThirdPersonController")
         {
-            SceneManager.LoadScene("StageSelect");
+            Clear = true;
         }
+    }
+
+    public bool GetClear()
+    {
+        return Clear;
     }
 }
