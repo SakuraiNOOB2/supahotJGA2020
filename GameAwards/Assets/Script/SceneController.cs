621aad3be7f74aefb4460416dadeae121a548fbe
Doge-Coin-Tsukae <63012757+Doge-Coin-Tsukae@users.noreply.github.com>
2020-05-28 12:51:42 +0900

    ステージフェード導入
    コンティニューから同じステージか選択画面に移行するときフェードする

--- GameAwards/Assets/Script/SceneController.cs
+++ GameAwards/Assets/Script/SceneController.cs
@@ -6,25 +6,51 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    bool Botton;           //ボタン押されたか
+    bool YesOrNo;       //true=Yes,false=No
+
+    [SerializeField]
+    private GameObject Fade;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Botton = false;
+        YesOrNo = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //ボタンが押された時に処理を行う
+        if (Botton == true)
+        {//フェードが終わったか
+            if (Fade.GetComponent<Fade>().GetFadeStart() == false)
+            {//押されたボタンで変える
+                if (YesOrNo == true)
+                {//リトライ(Yes)
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+                else
+                {//戻る(No)
+                    SceneManager.LoadScene("StageSelect");
+                }
+            }
+        }
     }
 
+    //YESボタンが押された
     public void YesButtonClicked()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Fade.GetComponent<Fade>().SetMode(1);
+        YesOrNo = true;
+        Botton = true;   
     }
 
+    //NOボタンが押された
     public void NoButtonClicked()
     {
-        SceneManager.LoadScene("StageSelect");
+        Fade.GetComponent<Fade>().SetMode(1);
+        YesOrNo = false;
+        Botton = true;
     }
 }
