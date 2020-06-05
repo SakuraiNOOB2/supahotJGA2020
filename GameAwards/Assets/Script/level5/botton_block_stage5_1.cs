0076b4b48aed4812bff6687fb0d5a3a6553190b9
LIUC342 <57204658+LIUC342@users.noreply.github.com>
2020-06-05 15:19:17 +0900

    stage5更新

---
+++ GameAwards/Assets/Script/level5/botton_block_stage5_1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botton_block_stage5_1 : MonoBehaviour
+{
+    public GameObject block;
+    bool switch_block;
+    // Start is called before the first frame update
+    void Start()
+    {
+        block = GameObject.Find("moveblock_botton1");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("concrete")&& block.transform.position.y < 8)
+        {
+            block.transform.position += transform.up * 0.1f;
+            
+        }
+    }
+}
