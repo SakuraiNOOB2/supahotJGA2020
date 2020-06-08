d515f577acbfb200bd7e8e45c0fa152eeafe4592
LIUC342 <57204658+LIUC342@users.noreply.github.com>
2020-06-08 13:43:48 +0900

    ドアのアニメーション追加
    BGMと新しいプレイヤーモデルも導入

---
+++ GameAwards/Assets/Script/level5/botton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botton : MonoBehaviour
+{
+    public bool switch_door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch_door = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("concrete") || other.CompareTag("Player"))
+        {
+            switch_door = true;
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("concrete") || other.CompareTag("Player"))
+        {
+            switch_door = false;
+
+        }
+    }
+}
---
+++ GameAwards/Assets/Script/level5/door.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    Animator animator;
+    public GameObject door_switch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("open", false);
+
+        if(door_switch.GetComponent<botton>().switch_door)
+        {
+            animator.SetBool("open", true);
+        }
+    }
+}
