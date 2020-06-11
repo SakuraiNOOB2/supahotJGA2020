47476fab621bff2500505176d96f3efa507b7d45
3162alf <63012681+3162alf@users.noreply.github.com>
2020-06-11 15:24:44 +0900

    viewCamera追加
    CameraControllを少しいじった
    viewCameraのプレハブを追加
    viewCamera.csはまだ途中

--- GameAwards/Assets/Script/CameraControll.cs
+++ GameAwards/Assets/Script/CameraControll.cs
@@ -6,8 +6,11 @@ public class CameraControll : MonoBehaviour
 {
     public subCamera sc;
 
+    private bool viewCameraIsOn;
+
     private GameObject mainCamera;
     private GameObject subCamera;
+    private GameObject viewCamera;
     private GameObject Timer;
     private GameObject Player;
 
@@ -17,6 +20,8 @@ public class CameraControll : MonoBehaviour
         //カメラを取得
         mainCamera = GameObject.Find("Camera");
         subCamera = GameObject.Find("subCamera");
+        viewCamera = GameObject.Find("viewCamera");
+
         Timer = GameObject.Find("TimeCount");
         Player = GameObject.Find("ThirdPersonController");
 
@@ -25,22 +30,59 @@ public class CameraControll : MonoBehaviour
         //プレイヤーと制限時間を一時停止
         Timer.SetActive(false);
         Player.GetComponent<PlayerControll>().enabled = false;
+
+
+        viewCameraIsOn = false;
+        viewCamera.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         //フラグが切り替わったらカメラも変更して、ゲームスタート
-        bool CameraSet;
-        CameraSet = sc.GetsubCameraIsOn();
+        bool subCameraSet;
+        subCameraSet = sc.GetsubCameraIsOn();
 
-        if(CameraSet==false)
+        //ボタンが押されたらviewCameraを切り替える
+        if(Input.GetKeyDown(KeyCode.Y))
+        {
+            viewCameraIsOn = !viewCameraIsOn;
+        }
+
+        //subCameraの処理が終わった後
+        if(subCameraSet==false)
         {
             subCamera.SetActive(false);
             mainCamera.SetActive(true);
             //プレイヤーと制限時間を起動
             Timer.SetActive(true);
             Player.GetComponent<PlayerControll>().enabled = true;
+
+
+            //ボタンが押されたら、ステージを見渡せるカメラに切り替える
+            //viewCameraの処理
+            if (viewCameraIsOn==true)
+            {
+                mainCamera.SetActive(false);
+                //Timer.SetActive(false);
+                Player.GetComponent<PlayerControll>().enabled = false;
+                viewCamera.SetActive(true);
+            }
+
+            if (viewCameraIsOn==false)
+            {
+                mainCamera.SetActive(true);
+                //Timer.SetActive(true);
+                Player.GetComponent<PlayerControll>().enabled = true;
+                viewCamera.SetActive(false);
+            }
+
+
+
         }
+
+
+
+
     }
 }
