e79746218eff55b26103f582c768bb8c8806af9e
Doge-Coin-Tsukae <63012757+Doge-Coin-Tsukae@users.noreply.github.com>
2020-06-13 19:13:11 +0900

    やっぱり消した
    更新してくれたので(Viewcameraの機能)PlayerControllから削除

--- GameAwards/Assets/Script/Player/PlayerControll.cs
+++ GameAwards/Assets/Script/Player/PlayerControll.cs
@@ -9,8 +9,6 @@ public class PlayerControll : MonoBehaviour
     private bool jumpflag;
     private bool breakfrag;
     public Rigidbody rb;
-    public GameObject maincamera;
-    public GameObject subcamera;
     Animator _animator;
     // Start is called before the first frame update
     void Start()
@@ -51,16 +49,6 @@ public class PlayerControll : MonoBehaviour
             _animator.SetBool("isRun", true);
         }
 
-        if (Input.GetKey("joystick button 3"))
-        {
-            maincamera.SetActive(false);
-            subcamera.SetActive(true);
-        }
-        else
-        {
-            subcamera.SetActive(false);
-            maincamera.SetActive(true);
-        }
 
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown("joystick button 0"))
         {
